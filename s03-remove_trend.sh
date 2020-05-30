1a70f6317fc5ef9f363fcca1ae1434bd6f85ca1c
MenakaRevel <menaka@rainbow.iis.u-tokyo.ac.jp>
2020-05-30 20:25:02 +0900

    added s03-remove_trend.sh

--- s03-remove_trend.sh
+++ s03-remove_trend.sh
@@ -1,19 +1,18 @@
 #!/bin/sh
 #====================
-# convert binary files to netCDF4 
-# all the time steps will be in one file
-# dimesnion (time, nx, ny)
+# Remove linear trend lines from the data
+# by fitting y=a*x+b
 # Menaka@IIS
 # 2020/05/29
 #====================
 #*** PBS setting when needed
 #PBS -q E20
-#PBS -l select=1:ncpus=20:mem=100gb
+#PBS -l select=1:ncpus=20:mem=40gb
 #PBS -j oe
 #PBS -m ea
 #PBS -M menaka@rainbow.iis.u-tokyo.ac.jp
 #PBS -V
-#PBS -N bin2nc
+#PBS -N rm_trd
 #========
 cd $PBS_O_WORKDIR
 #================================================
@@ -22,7 +21,7 @@ cd $PBS_O_WORKDIR
 syear=`python -c "import params; print (params.starttime()[0])"`
 smonth=`python -c "import params; print (params.starttime()[1])"`
 sdate=`python -c "import params; print (params.starttime()[2])"`
-eyear=2010 #`python -c "import params; print (params.endtime()[0])"`
+eyear=1999 #`python -c "import params; print (params.endtime()[0])"`
 emonth=12 #`python -c "import params; print (params.endtime()[1])"`
 edate=31 #`python -c "import params; print (params.endtime()[2])"`
 echo $syear" to "$eyear
@@ -33,6 +32,5 @@ mapname=`python -c "import params; print (params.map_name())"`
 inputname=`python -c "import params; print (params.input_name())"`
 N=`python src/calc_days.py $syear $smonth $sdate $eyear $emonth $edate`
 varname="sfcelv"
-#echo $N
 #=================================================
-./src/bin2nc $N $syear $eyear $varname $mapname $inputname $CAMADIR $outdir
+./src/remove_trend $N $syear $eyear $varname $mapname $inputname $CAMADIR $outdir
---
+++ src/remove_trend.f90
@@ -0,0 +1,293 @@
+program trend_sfcelv
+!===================================================
+! calcualte the trend line using y=a*x+b
+! Menaka@IIS
+! files read and write using netCDF4 format
+! 2020/0530
+! Reference:
+! Revel et al,. (2019), Revel et al,. (2019)
+!====================================================
+!$ use omp_lib
+use netcdf
+implicit none
+character(len=128)                    :: fname,buf,camadir,outdir
+character(len=32)                     :: outname,varname,mapname,inname
+integer                               :: syear,eyear
+integer                               :: smonth,sday,shour,smin
+character(len=128)                    :: longname,tag,ctime
+character(len=8)                      :: units
+!-map variables
+real                                  :: gsize,west, north, east, south ! map boundries
+integer                               :: latpx,lonpx,nflp    ! pixel size, calculated
+real,allocatable                      :: globaltrue(:),rmdtrnd(:)
+integer,allocatable                   :: nextX(:,:),nextY(:,:),ocean(:,:)
+integer                               :: i,ios,N
+real,allocatable                      :: xt(:)
+real                                  :: a,b
+integer                               :: timeid,varid,latid,lonid 
+integer                               :: ncidin,ncidout,varidin,varidout
+integer                               :: ix,iy,nx,ny
+integer,allocatable                   :: dt(:)
+real,allocatable                      :: d1lat(:), d1lon(:)
+integer,dimension(3)                  :: start,count
+real,parameter                        :: rmis=1.0e20
+!====================================================
+call getarg(1,buf)
+read(buf,*) N ! length of time series
+
+call getarg(2,buf)
+read(buf,*) syear ! start year
+
+call getarg(3,buf)
+read(buf,*) eyear ! end year
+
+call getarg(4,buf)
+read(buf,'(A)') outname ! variable name [e.g. sfcelv,rivout,outflw]
+
+call getarg(5,buf)
+read(buf,"(A)") mapname ! map name
+
+call getarg(6,buf)
+read(buf,'(A)') inname ! input runoff forcing name
+
+call getarg(7,buf)
+read(buf,"(A)") camadir
+write(*,*) camadir
+
+call getarg(8,buf)
+read(buf,"(A)") outdir
+write(*,*) outdir
+!-
+varname=outname
+!==
+fname=trim(camadir)//"/map/"//trim(mapname)//"/params.txt"
+print *, fname
+open(11,file=fname,form='formatted')
+read(11,*) lonpx
+read(11,*) latpx
+read(11,*) nflp
+read(11,*) gsize
+read(11,*) west
+read(11,*) east
+read(11,*) south
+read(11,*) north
+close(11)
+!-------
+!======
+allocate(nextX(lonpx,latpx),nextY(lonpx,latpx),ocean(lonpx,latpx))
+! read next grid information
+! read nextX and nextY
+fname=trim(adjustl(camadir))//"map/glb_15min/nextxy.bin"
+open(34,file=fname,form="unformatted",access="direct",recl=4*latpx*lonpx,status="old",iostat=ios)
+if(ios==0)then
+    read(34,rec=1) nextX
+    read(34,rec=2) nextY
+else
+    write(*,*) "no file nextXY at:",fname    
+end if
+close(34)
+
+! make ocean mask from storage data (1 is ocean; 0 is not ocean)
+ocean = (nextX == -9999) * (-1)
+
+!*********************************
+!============WRITING==============
+!*********************************
+!  ncidout
+!  varidout
+!---------------------------------
+! write netCDF file
+!******
+!https://www.unidata.ucar.edu/software/netcdf/examples/programs/ 
+!******
+! Create the netCDF file
+nx=lonpx
+ny=latpx-30.0/dble(gsize) ! writed only up -60S latitude
+write(tag,'(i4.0,a,i4.0)')syear,"-",eyear
+fname=trim(adjustl(outdir))//"/CaMa_out/"//trim(inname)//"/rmdtrnd"//trim(tag)//".nc"
+print*, "create",fname
+call nccheck( nf90_create(fname, NF90_NETCDF4, ncidout) )
+!=== set dimension ===
+print*, "set dimension"
+call nccheck( nf90_def_dim(ncidout, 'time', NF90_UNLIMITED, timeid) )
+call nccheck( nf90_def_dim(ncidout, 'lat', ny, latid) )
+call nccheck( nf90_def_dim(ncidout, 'lon', nx, lonid) )
+!=== define variables ===
+print*, "define variables"
+call nccheck( nf90_def_var(ncidout, 'lat', nf90_float, (/latid/), varid) )
+call nccheck( nf90_put_att(ncidout, varid, 'long_name','latitude') )
+call nccheck( nf90_put_att(ncidout, varid, 'units','degrees_north') )
+
+call nccheck( nf90_def_var(ncidout, 'lon', nf90_float, (/lonid/), varid) )
+call nccheck( nf90_put_att(ncidout, varid, 'long_name','longitude') )
+call nccheck( nf90_put_att(ncidout, varid, 'units','degrees_east') )
+! Create time unit
+print*, "create variables metadata"
+smonth=1
+sday=1
+shour=0
+smin=0
+write(ctime,'(a14,i4.4,a1,i2.2,a1,i2.2,a1,i2.2,a1,i2.2)') 'days since ',syear,'-',smonth,'-',sday,' ',shour,":",smin
+print*, "time"
+call nccheck( nf90_def_var(ncidout, 'time', nf90_int, (/timeid/), varid) ) 
+call nccheck( nf90_put_att(ncidout, varid, 'long_name','time') )
+call nccheck( nf90_put_att(ncidout, varid, 'units',ctime) )
+
+!===
+print*, "variable"
+call nccheck( nf90_def_var(ncidout, "rmdtrnd", nf90_float, &
+         (/lonid,latid,timeid/), varidout) )
+print*, "put attribute"
+call nccheck( nf90_put_att(ncidout, varidout, 'long_name', trim(longname(varname))) )
+call nccheck( nf90_put_att(ncidout, varidout, 'units',     trim(units(varname))) )
+call nccheck( nf90_put_att(ncidout, varidout, '_fillvalue',rmis) )
+
+!===end header===
+print*, "end def"
+call nccheck( nf90_enddef(ncidout) )
+
+!=== put lon lat info ===
+print*, "put lon, lat info"
+allocate(d1lat(ny),d1lon(nx))
+do ix=1,nx
+  d1lon(ix)=west +(dble(ix)-0.5d0)*gsize !(east-west)  /dble(nx)
+enddo
+do iy=1,ny
+  d1lat(iy)=north-(dble(iy)-0.5d0)*gsize !(north-(-60.0))/dble(ny)
+enddo
+
+call nccheck( nf90_inq_varid(ncidout,'lon',varid) )
+call nccheck( nf90_put_var(ncidout,varid,d1lon))
+
+call nccheck( nf90_inq_varid(ncidout,'lat',varid) )
+call nccheck( nf90_put_var(ncidout,varid,d1lat))
+
+!===put time===
+print*, "put time"
+allocate(dt(N))
+dt = (/(i, i=1,N,1)/)! days since syear, smonth, sday
+
+call nccheck( nf90_inq_varid(ncidout,'time',varid) )
+call nccheck( nf90_put_var(ncidout,varid,dt) )
+
+!===============
+!*********************************
+!============READING==============
+!*********************************
+!  ncidin
+!  varidin
+!---------------------------------
+! read netCDF file
+fname=trim(outdir)//"/CaMa_out/"//trim(inname)//"/"//trim(varname)//trim(tag)//".nc"
+call nccheck( nf90_open(fname, nf90_nowrite, ncidin) )
+call nccheck( nf90_inq_varid(ncidin, trim(varname),varidin) )
+allocate(globaltrue(N),rmdtrnd(N),xt(N))
+xt = (/(real(i), i=1,N,1)/)
+
+
+! parallel calculation
+
+!$omp parallel default(private) shared(ocean,rivwth,globaltrue,xt,N)
+!$omp do
+!$write(*,*) omp_get_num_threads()
+do ix = 1,nx ! pixels along longtitude direction
+    do iy = 1,ny ! pixel along latitude direction
+        start=(/ix,iy,1/)
+        count=(/1,1,N/)
+        !remove ocean
+        if (ocean(ix,iy) /= 0) then
+            globaltrue = 1e20
+            rmdtrnd(:) = 1e20
+        else
+            ! get variable subset
+            call nccheck( nf90_get_var(ncidin,varidin,globaltrue,start=start,count=count) )
+            !--trend line as y = a + bx
+            call trend_para(globaltrue,N,a,b)
+            !---
+            rmdtrnd(:) = globaltrue - (a + b*(xt))
+        end if
+        !--write variable--
+        call nccheck( nf90_put_var(ncidout,varidout,rmdtrnd,start=start,count=count) )
+    end do
+end do
+!$write(*,*) omp_get_num_threads() 
+!$omp end do
+!$omp end parallel
+!---
+!====close netcdf=====
+call nccheck( nf90_close(ncidin ) )
+call nccheck( nf90_close(ncidout) )
+!---
+deallocate(nextX,nextY,ocean,globaltrue,xt,rmdtrnd)
+end program trend_sfcelv
+!************************
+subroutine trend_para(y,N,a,b)
+implicit none
+!--
+integer                :: N,i
+real,dimension(N)      :: y,x
+real                   :: a,b 
+!---
+x = (/(real(i), i=1,N,1)/)
+
+b = (N*sum(y*x)-(sum(y)*sum(x)))/(n*sum(x**2)-sum(x)**2)
+a = sum(y)/N - b*sum(x)/N
+return
+end subroutine trend_para
+!*********************************
+function dyear(year)
+implicit none
+integer                        :: year,dyear
+!real                           :: mod
+!--
+! days of the year
+! normal : 365
+! leap   : 366
+dyear=365
+if (mod(dble(year),4.0) == 0) then
+    dyear=366
+endif
+return
+end function dyear
+!*********************************
+subroutine nccheck(status)
+use netcdf
+implicit none
+integer, intent ( in)          :: status
+!=============================
+if(status /= nf90_noerr) then
+  print *, trim(nf90_strerror(status))
+  stop "Stopped"
+end if
+end subroutine nccheck
+!*********************************
+function units(varname)
+implicit none
+character(len=32)                 :: varname
+character(len=8)                  :: units
+if (trim(varname)=="sfcelv") then
+    units="m"
+end if
+if (trim(varname)=="outflw" .or. trim(varname)=="rivout") then
+    units="m3/s"
+end if
+return
+end function units
+!*********************************
+function longname(varname)
+implicit none
+character(len=32)                 :: varname
+character(len=128)                :: longname
+if (trim(varname)=="sfcelv") then
+    longname="water surface elevation"
+end if
+if (trim(varname)=="outflw") then
+    longname="total discharge (river+floodplain)"
+end if
+if (trim(varname)=="rivout") then
+    longname="river discharge"
+end if
+
+return
+end function longname
+!*********************************
