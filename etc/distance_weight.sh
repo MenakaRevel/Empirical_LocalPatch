e44bd22d90fe167689b437c878e6068cf4d30314
MenakaRevel <menakarevel@gmail.com>
2023-01-19 17:00:57 +0900

    add etc folder

---
+++ etc/distance_weight.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+#====================
+# Calcualte the experimental semivarinces along each river stem
+# Menaka@IIS
+# 2020/06/01
+#====================
+#*** PBS setting when needed
+#PBS -q F40
+#PBS -l select=1:ncpus=40:mem=40gb
+#PBS -j oe
+#PBS -m ea
+#PBS -M menaka@rainbow.iis.u-tokyo.ac.jp
+#PBS -V
+#PBS -N distance_weight
+#========
+# import virtual environment
+source ~/.bashrc
+source ~/.bash_conda
+
+source activate pydef
+
+which python
+#========
+# cd $PBS_O_WORKDIR
+cd "/cluster/data6/menaka/Empirical_LocalPatch/etc"
+#================================================
+# OpenMP Thread number
+NCPUS=40
+export OMP_NUM_THREADS=$NCPUS
+
+# input settings
+syear=`python -c "import params; print (params.starttime()[0])"`
+smonth=`python -c "import params; print (params.starttime()[1])"`
+sdate=`python -c "import params; print (params.starttime()[2])"`
+eyear=`python -c "import params; print (params.endtime()[0])"`
+emonth=`python -c "import params; print (params.endtime()[1])"`
+edate=`python -c "import params; print (params.endtime()[2])"`
+echo $syear" to "$eyear
+CAMADIR=`python -c "import params; print (params.CaMa_dir())"`
+outdir=`python -c "import params; print (params.out_dir())"`
+cpunums=$NCPUS #`python -c "import params; print (params.cpu_nums())"`
+mapname=`python -c "import params; print (params.map_name())"`
+inputname=`python -c "import params; print (params.input_name())"`
+N=`python src/calc_days.py $syear $smonth $sdate $eyear $emonth $edate`
+threshold=`python -c "import params; print (params.threshold())"`
+# represnt dams
+damrep=`python -c "import params; print (params.dam_rep())"`
+#=================================================
+python src/weightage.py $CAMADIR $mapname $inputname $outdir $cpunums $threshold $damrep &
+
+wait
+
+conda deactivate
---
+++ etc/regonalize_CaMa.sh
@@ -0,0 +1,71 @@
+#!/bin/sh
+#====================
+# Use externally simulated CaMa-Flood results to create Emperical Local Patches
+# Convert global data to regional netCDF
+# Better for high resoultion CaMa maps [e.g., 06min]
+# Menaka@IIS
+# 2023/01/18
+#====================
+#*** PBS setting when needed
+#PBS -q E20
+#PBS -l select=1:ncpus=20:mem=100gb
+#PBS -j oe
+#PBS -m ea
+#PBS -M menaka@rainbow.iis.u-tokyo.ac.jp
+#PBS -V
+#PBS -N regonalize
+#===========================
+# cd $PBS_O_WORKDIR
+cd "/cluster/data6/menaka/Empirical_LocalPatch/etc"
+
+#================================================
+# OpenMP Thread number
+NCPUS=40
+export OMP_NUM_THREADS=$NCPUS
+
+# link params.py
+rm -r params.py
+ln -sf ../params.py params.py
+
+# input settings
+syear=`python -c "import params; print (params.starttime()[0])"`
+smonth=`python -c "import params; print (params.starttime()[1])"`
+sdate=`python -c "import params; print (params.starttime()[2])"`
+eyear=`python -c "import params; print (params.endtime()[0])"`
+emonth=`python -c "import params; print (params.endtime()[1])"`
+edate=`python -c "import params; print (params.endtime()[2])"`
+echo $syear" to "$eyear
+CAMADIR=`python -c "import params; print (params.CaMa_dir())"`
+outdir=`python -c "import params; print (params.out_dir())"`
+# cpunums=`python -c "import params; print (params.cpu_nums())"`
+cpunums=$NCPUS
+mapname=`python -c "import params; print (params.map_name())"`
+inputname=`python -c "import params; print (params.input_name())"`
+N=`python src/calc_days.py $syear $smonth $sdate $eyear $emonth $edate`
+threshold=`python -c "import params; print (params.threshold())"`
+
+patch=100
+
+# global map name
+glbmapname="glb_06min"
+
+# link simulated files
+cd "../CaMa_out/${mapname}_${inputname}"
+ln -sf "/home/yamadai/data/CaMa_v400_simulations/VIC_BC_3h_06min/* ."
+cd ../../etc
+
+#======
+# create folder name in CaMa_out
+mkdir -p "../CaMa_out/${mapname}_${inputname}"
+
+# water surface elevation
+varname="sfcelv"
+#=================================================
+echo ./src/bin2nc_reg $N $syear $eyear $varname $mapname $glbmapname $inputname $CAMADIR $outdir &
+# time ./src/bin2nc_reg $N $syear $eyear $varname $mapname $glbmapname $inputname $CAMADIR $outdir &
+
+# discharge
+varname="outflw"
+#=================================================
+echo ./src/bin2nc_reg $N $syear $eyear $varname $mapname $glbmapname $inputname $CAMADIR $outdir &
+# time ./src/bin2nc_reg $N $syear $eyear $varname $mapname $glbmapname $inputname $CAMADIR $outdir &
